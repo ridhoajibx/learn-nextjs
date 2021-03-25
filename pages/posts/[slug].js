5d4ed1accd13dc62f2904653660b3ccf74ecbcdd
ridhoajibx <ridhoajibx@gmail.com>
2021-03-25 21:06:41 +0700

    [ridho] - create post details with getStaticPaths

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,16 +1,53 @@
 import { useRouter } from 'next/router';
 import Layout from '../../components/Layout';
 
-export default function Show() {
+export default function Show({ post }) {
 
     const router = useRouter();
     const { slug } = router.query
-    console.log(slug);
+    console.log(post);
     return (
         <Layout title="Posts">
             <div className="container">
-                This wildcard is : {slug}
+                <div>
+                    <h3 className="font-bold text-2xl border-b pb-2">
+                        { post.title }
+                    </h3>
+                    <div className="py-4">
+                        { post.body }
+                    </div>
+                    <div>
+                        Author : <strong>{ post.user.name }</strong>
+                    </div>
+                    <div>
+                        Published on <strong>{ post.created_at }</strong>
+                    </div>
+                </div>
             </div>
         </Layout>
     )
+}
+
+export const getStaticPaths = async () => {
+    const response = await fetch("http://localhost:8000/api/posts/");
+    const posts = await response.json()
+
+    const paths = posts.data.map((post) => (
+        {
+            params: { slug: post.slug }
+        }
+    ));
+
+    return { paths, fallback: false };
+}
+
+export const getStaticProps = async (context) => {
+    const response = await fetch(`http://localhost:8000/api/posts/${context.params.slug}`);
+    const post = await response.json()
+
+    return {
+        props: {
+            post: post.data
+        }
+    }
 }
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,16 +1,23 @@
+import Link from 'next/link';
 import Layout from '../../components/Layout';
 
 export default function Index(props) {
     return (
         <Layout title="Posts">
             <div className="container">
-                {
-                    props.posts.map(post => (
-                        <div key={post.id}>
-                            { post.title }
-                        </div>
-                    ))
-                }
+                <div className="flex">
+                    <div className="w-1/2">
+                        {
+                            props.posts.map(post => (
+                                <Link href={`/posts/${post.slug}`} key={post.id}>
+                                    <a className="block border-gray-500 hover:bg-blue-500 hover:text-white p-4 rounded shadow mb-2" key={post.id}>
+                                        { post.title}
+                                    </a>
+                                </Link>
+                            ))
+                        }
+                    </div>
+                </div>
             </div>
         </Layout>
     )
@@ -19,7 +26,7 @@ export default function Index(props) {
 export const getStaticProps = async () => {
     const response = await fetch("http://localhost:8000/api/posts/");
     const posts = await response.json()
-    
+
     return {
         props: {
             posts: posts.data
